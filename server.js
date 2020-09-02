5c1b1b1837f349a1ae51665f67c7b9515e4cacb4
wangze <wangze1@corp.netease.com>
2020-09-02 12:38:55 +0800

    feat: add js & webpack

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const Koa = require('koa');
+const path = require('path');
 const fs = require('fs')
+const Koa = require('koa');
+const static = require('koa-static');
+const Router = require('koa-router');
+
+let home = new Router();
+
+home.get('/home', async ctx => {
+  ctx.response.type = 'html'
+  ctx.body = fs.createReadStream('./index.html');
+})
 
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.response.type = 'html'
-  ctx.body = fs.createReadStream('./index.html');
-});
+// 配置静态资源
+const staticPath = 'dist'
+app.use(static(
+    path.join(__dirname, staticPath)
+))
+
+app.use(home.routes(), home.allowedMethods());
+
 
 app.listen(3000);
